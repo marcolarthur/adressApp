12c4c27c3d078ed8ad8af641a46c537d2da89c15
Arthur Marcolino <arthurfm@live.com>
2019-10-01 11:08:50 -0300

    V 0.2

--- versao3bimestre/util/Interface/ILocalDateAdapter.java
+++ versao3bimestre/util/Interface/ILocalDateAdapter.java
@@ -1,1 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.makery.util.Interface;
 
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Aluno
+ */
+public interface ILocalDateAdapter {
+
+    String marshal(LocalDate v) throws Exception;
+
+    LocalDate unmarshal(String v) throws Exception;
+    
+}
