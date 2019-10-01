53fdbd94f9b998df419e591d573e17a6f91f7d0f
Arthur Marcolino <arthurfm@live.com>
2019-10-01 11:07:15 -0300

    V 0.1

--- versao3bimestre/view/Interface/IFXChart.java
+++ versao3bimestre/view/Interface/IFXChart.java
@@ -1,1 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.makery.view.Interface;
 
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+
+public interface IFXChart {
+    
+    public CategoryAxis createChart(CategoryAxis xAxis, String[] categories);
+    public BarChart<String, Integer> fillChart(BarChart<String, Integer> barChart, int[] monthCounter);
+}
